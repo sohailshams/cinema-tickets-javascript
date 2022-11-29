65c360886f4b1f4de51954187aa18a68856f8061
sohailshams <sohailshamas@yahoo.com>
2022-11-29 14:58:06 +0000

    Created constants file.

---
+++ cinema-tickets-javascript/src/pairtest/constants.js
@@ -0,0 +1,5 @@
+export const ADULTTICKETPRICE = 20;
+export const CHILDTICKETPRICE = 10;
+export const ADULTTICKETTYPE = 'ADULT';
+export const CHILDTICKETTYPE = 'CHILD';
+export const TICKETDATAOBJ = {'totalAmountToPay': 0, 'totalTickets': 0}
